945f9016fbf5badff874ff590fc1db9d120a916c
pawan-meena <pawanmeenazee@outlook.com>
2024-12-20 00:38:25 +0530

    done

--- generate-otp.php
+++ generate-otp.php
@@ -1,28 +1,48 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
-
 use OTPHP\TOTP;
 use Symfony\Component\Clock\Clock;
-use Endroid\QrCode\Builder\Builder;
-use Endroid\QrCode\Encoding\Encoding;
-use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelHigh;
-use Endroid\QrCode\Writer\PngWriter;
 $clock = new Clock();
 $otp = TOTP::generate($clock);
 $secret = $otp->getSecret();
 $otp->setLabel('PAWAN_APP');
 
+
 $otpSecrets = file_exists('otp-secrets.json') ? json_decode(file_get_contents('otp-secrets.json'), true) : [];
+
+
+$userId = $_GET['user_id'];
+$existingIndex = null;
+
+foreach ($otpSecrets as $index => $entry) {
+    if ($entry['user_id'] == $userId) {
+        $existingIndex = $index;
+        break;
+    }
+}
+
+
 $grCodeUri = $otp->getQrCodeUri(
-    'https://api.qrserver.com/v1/create-qr-code/?data='.$secret.'&size=300x300&ecc=M',
+    'https://api.qrserver.com/v1/create-qr-code/?data=' . $secret . '&size=300x300&ecc=M',
     $secret
 );
-$otpSecrets[] = [
-    'user_id' => $_GET['user_id'],
-    'secret' => $secret,
-    'qr_link' => $grCodeUri ,
-    'created_at' => time()
-];
+
+
+if ($existingIndex !== null) {
+
+    $otpSecrets[$existingIndex]['secret'] = $secret;
+    $otpSecrets[$existingIndex]['qr_link'] = $grCodeUri;
+    $otpSecrets[$existingIndex]['created_at'] = time();
+} else {
+  
+    $otpSecrets[] = [
+        'user_id' => $userId,
+        'secret' => $secret,
+        'qr_link' => $grCodeUri,
+        'created_at' => time()
+    ];
+}
+
 
 file_put_contents('otp-secrets.json', json_encode($otpSecrets, JSON_PRETTY_PRINT));
 
